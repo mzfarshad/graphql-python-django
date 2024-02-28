75306e1ed5281af4bf8570d546dfdd5fb1fa97f5
mzfarshad <farshad.akbari.arzati@gmail.com>
2024-02-28 18:14:56 +0330

    modify one.py

--- one.py
+++ one.py
@@ -6,14 +6,16 @@ import json
 
 class User(graphene.ObjectType):
     id = graphene.ID(default_value=uuid.uuid4())
-    username = graphene.String()
     created_at = graphene.DateTime(default_value=datetime.now())
+    username = graphene.String()
     email = graphene.String()
+    password = graphene.String()
 
 
 class UserInput(graphene.InputObjectType):
     username = graphene.String(required=True)
     email = graphene.String(required=True)
+    password = graphene.String(required=True)
 
 
 class CreateUser(graphene.Mutation):
@@ -23,7 +25,7 @@ class CreateUser(graphene.Mutation):
         user_data = UserInput(required=True)
 
     def mutate(self, info, user_data):
-        user = User(username=user_data.username, email=user_data.email )
+        user = User(username=user_data.username, email=user_data.email, password=user_data.password)
         return CreateUser(user=user)
 
 
@@ -32,25 +34,22 @@ class Mutation(graphene.ObjectType):
 
 
 schema = graphene.Schema(query=User, mutation=Mutation)
-
 result = schema.execute("""
     mutation {
-        createUser(userData:{
-            username:"kevin",
-            email:"kevin@email.com"
-        }) {
-            user {
+        createUser( userData:{
+        username:"kevin",
+        email:"kevin@email.com",
+        password:"kevin123"
+        } ){
+            user{
                 id
-                username
-                email
                 createdAt
+                username
+                email       
             }
         }
     }
 """)
 
-print(result.data)
-
-j = json.dumps(dict(result.data), indent=2)
-
+j = json.dumps(result.data, indent=2)
 print(j)
