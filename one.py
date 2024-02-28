c353ece0e5f1a66ad57b008292550e2a472b7586
mzfarshad <farshad.akbari.arzati@gmail.com>
2024-02-28 15:47:53 +0330

    learn mutation

--- one.py
+++ one.py
@@ -1,29 +1,46 @@
 import graphene
+import uuid
+from datetime import datetime
 import json
 
 
-class Query(graphene.ObjectType):
-    name = graphene.String(username=graphene.String(default_value='farshad'))
+class User(graphene.ObjectType):
+    id = graphene.ID(default_value=uuid.uuid4())
+    username = graphene.String()
+    created_at = graphene.DateTime(default_value=datetime.now())
 
-    def resolve_name(self, info, username):
-        return f'Hello {username}'
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(User)
+
+    class Arguments:
+        userName = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        user = User(username=kwargs["userName"])
+        return CreateUser(user=user)
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+
+schema = graphene.Schema(query=User, mutation=Mutation)
+
 result = schema.execute("""
-    query{
-        name(username:"mahour")
+    mutation {
+        createUser(userName:"jack") {
+            user {
+                id
+                username
+                createdAt
+            }
+        }
     }
 """)
 
-print(result)
-print('===>> result is a dictionary <<===')
 print(result.data)
-print(result.data.values())
-print(result.data.keys())
-print(result.data['name'])
 
 j = json.dumps(dict(result.data), indent=2)
+
 print(j)
-
-
