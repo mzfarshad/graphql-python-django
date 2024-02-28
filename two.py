b729e8566fca4bdc08a853615409cce590956613
mzfarshad <farshad.akbari.arzati@gmail.com>
2024-02-28 18:28:30 +0330

    Learning to use context

--- two.py
+++ two.py
@@ -5,14 +5,14 @@ class Query(graphene.ObjectType):
     name = graphene.String(username=graphene.String(default_value="World"))
 
     def resolve_name(self, info, username):
-        return f"Hello {username}"
+        return f"Hello {info.context.get('user')}"
 
 
 schema = graphene.Schema(query=Query)
 result = schema.execute("""
     query{
-        name(username:"jan")
+        name
     }
-""")
+""", context={"user": "alex"})
 
 print(result.data)
